9363c80eb088dd7df54178eb82e6115b0983e9ed
Kevin <1439017+kevindoran@users.noreply.github.com>
2022-09-27 10:15:07 +0100

    Switch back to Davis & Frank batch norm initialization.

--- retinapy/src/retinapy/nn.py
+++ retinapy/src/retinapy/nn.py
@@ -15,9 +15,12 @@ def create_batch_norm(n):
     I tried both, and found better results with fastai's defaults.
     """
     bn = nn.BatchNorm1d(n)
-    #bn.weight.data.fill_(0.1)
-    bn.weight.data.fill_(1.0)
-    bn.bias.data.fill_(1e-3)
+    # fastai 
+    #bn.weight.data.fill_(1.0)
+    #bn.bias.data.fill_(1e-3)
+    # Davis and Frank
+    bn.weight.data.fill_(0.1)
+    bn.bias.data.fill_(0)
     return bn
 
 
