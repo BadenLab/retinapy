ccbea255e73e054230cb485d9d577ce055d352d2
Kevin <1439017+kevindoran@users.noreply.github.com>
2022-11-15 15:48:04 +0000

    Version up.

--- retinapy/setup.py
+++ retinapy/setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='retinapy',
-    version='0.0.1.dev8',
+    version='0.0.1.dev9',
     author='Baden Lab members',
     author_email='kevin@kdoran.com',
     description=('RetinaPy is a Python package for working with recordings of '
         'retina activity'),
     packages=['retinapy'],
     install_requires=[
+        'torch',
         'numpy',
         'pandas',
         'scipy',
